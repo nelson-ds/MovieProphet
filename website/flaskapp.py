f38449652f7920491467a8c10f3eb028cfff5bc5
nelson-ds <nelsonds@uw.edu>
2017-05-07 00:01:30 -0700

    Linked front-end to Back-end

--- website/flaskapp.py
+++ website/flaskapp.py
@@ -1,6 +1,63 @@
 from flask import Flask, render_template, request, jsonify, json
-import sys
+import sys, pymysql
 app = Flask(__name__)
+
+
+connection = pymysql.connect(host='localhost', user='root', password='toor', db='movies')
+cur = connection.cursor()
+
+cur.execute("select * FROM scores_act")
+act_sql = []
+for row in cur: act_sql.append(list(row))
+
+cur.execute("select * FROM scores_dir")
+dir_sql = []
+for row in cur: dir_sql.append(list(row))
+
+cur.execute("select * FROM scores_pro")
+pro_sql = []
+for row in cur: pro_sql.append(list(row))
+
+cur.execute("select * FROM scores_wri")
+wri_sql = []
+for row in cur: wri_sql.append(list(row))
+
+cur.execute("select * FROM scores_cin")
+cin_sql = []
+for row in cur: cin_sql.append(list(row))
+
+cur.execute("select * FROM scores_com")
+com_sql = []
+for row in cur: com_sql.append(list(row))
+
+cur.execute("select * FROM scores_dis")
+dis_sql = []
+for row in cur: dis_sql.append(list(row))
+
+cur.close()
+connection.close()
+
+k_act = [item[0] for item in act_sql]
+v_act = [item[1] for item in act_sql]
+
+k_dir = [item[0] for item in dir_sql]
+v_dir = [item[1] for item in dir_sql]
+
+k_pro = [item[0] for item in pro_sql]
+v_pro = [item[1] for item in pro_sql]
+
+k_wri = [item[0] for item in wri_sql]
+v_wri = [item[1] for item in wri_sql]
+
+k_cin = [item[0] for item in cin_sql]
+v_cin = [item[1] for item in cin_sql]
+
+k_com = [item[0] for item in com_sql]
+v_com = [item[1] for item in com_sql]
+
+k_dis = [item[0] for item in dis_sql]
+v_dis = [item[1] for item in dis_sql]
+
 
 @app.route('/')
 def index():
@@ -8,19 +65,59 @@ def index():
 
 @app.route('/prediction/')
 def prediction():
-    keys = ['Johnny Depp', 'Meryl Streep', 'Leonardo DiCaprio', 'Matt Damon', 'Benedict Cumberbatch', 'Anne Hathaway', 'Christian Bale',
-    'Scarlett Johansson', 'Hugh Jackman', 'Michael Caine']
-    values = [1,2,3,4,5,6,7,8,9,10]
-
-    data = []
-    for i in range(len(keys)):
+    data_act = []
+    for i in range(len(k_act)):
         tmp={}
-        tmp["id"] = values[i]
-        tmp["text"] = keys[i]
-        data.append(tmp)
-    print(json.dumps(data))
+        tmp["id"] = v_act[i]
+        tmp["text"] = k_act[i]
+        data_act.append(tmp)
+        # print(json.dumps(data_dir))
 
-    return render_template('prediction.html', data=json.dumps(data))
+    data_dir = []
+    for i in range(len(k_dir)):
+        tmp={}
+        tmp["id"] = v_dir[i]
+        tmp["text"] = k_dir[i]
+        data_dir.append(tmp)
+
+    data_pro = []
+    for i in range(len(k_pro)):
+        tmp={}
+        tmp["id"] = v_pro[i]
+        tmp["text"] = k_pro[i]
+        data_pro.append(tmp)
+
+    data_wri = []
+    for i in range(len(k_wri)):
+        tmp={}
+        tmp["id"] = v_wri[i]
+        tmp["text"] = k_wri[i]
+        data_wri.append(tmp)
+
+    data_cin = []
+    for i in range(len(k_cin)):
+        tmp={}
+        tmp["id"] = v_cin[i]
+        tmp["text"] = k_cin[i]
+        data_cin.append(tmp)
+
+    data_com = []
+    for i in range(len(k_com)):
+        tmp={}
+        tmp["id"] = v_com[i]
+        tmp["text"] = k_com[i]
+        data_com.append(tmp)
+
+    data_dis = []
+    for i in range(len(k_dis)):
+        tmp={}
+        tmp["id"] = v_dis[i]
+        tmp["text"] = k_dis[i]
+        data_dis.append(tmp)
+
+    return render_template('prediction.html', data_act=json.dumps(data_act), data_dir=json.dumps(data_dir), 
+        data_pro=json.dumps(data_pro), data_wri=json.dumps(data_wri), data_cin=json.dumps(data_cin),
+        data_com=json.dumps(data_com), data_dis=json.dumps(data_dis))
 
 @app.route('/chart/')
 def chart():
@@ -34,16 +131,14 @@ def table():
 def return_revenue():
     moviename = request.args.get('f_moviename', 'N/A')
     budget = request.args.get('f_budget', 0, type=int)
-    production = request.args.get('f_production', 'N/A')
-    actors = request.args.getlist('f_actors[]')
-    actors = [int(i) for i in actors]
+    actors = request.args.getlist('f_act[]')
+    actors = [float(i) for i in actors]
     print('actors are', actors)
 
     rev = 0 
     if budget < 1000: 
     	rev+=-2000
     else: rev+=5000
-    if production == 'Disney': rev+=5
     rev+=sum(actors)
 
     return jsonify(result=rev, mname=moviename, bdgt=budget, act=actors)
